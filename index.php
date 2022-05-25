684103e104304cbadba2af1a18c2c4a50cd1fd30
elisachiriacogit config --global user.email <elisachiriaco22@outlook.it>
2022-05-25 16:30:37 +0200

    milestone 1

--- index.php
+++ index.php
@@ -14,25 +14,25 @@ include __DIR__.'/data.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- vue -->
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="./css/style.css">
     <title>PHP Dischi</title>
 </head>
 <body>
     <header>
-        <img src="../assets/logo.png" alt="">
+        <img src="./img/logo.png" alt="">
     </header>
     <main>
         <div class="container">
-            <div class="row">
-                <div class="col-2">
-                    <?php foreach($dischi as $disco){ ?> 
-                        <img src="<?php echo $disco['poster']?>" alt="">
-                        <p><?php echo $disco['title'] ?></p>
-                        <p><?php echo $disco['author'] ?></p>
-                        <p><?php echo $disco['genre'] ?></p>
-                        <p><?php echo $disco['year'] ?></p>
-                    <?php } ?>
-                 </div>
+            <div class="row justify-content-center p-4">
+            <?php foreach($dischi as $disco){ ?>
+                <div class="col-6 col-sm-4 col-md-2 m-2">
+                <div class="card-container justify-content-center">
+                    <img src="<?php echo $disco['poster']?>" alt="">
+                    <h4><?php echo $disco['title'] ?></h4>
+                    <p><?php echo $disco['author'] ?> <br> <?php echo $disco['year'] ?> </p>
+                </div> 
+                </div>
+                <?php } ?>
             </div>
         </div>
     </main>
